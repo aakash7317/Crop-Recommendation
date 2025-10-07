6b7993c7db8b16f37ac471a5688d7bfff62f6dd4
aakash7317 <aakashmishra73170317@gmail.com>
2025-10-07 22:44:23 +0530

    Initial commit with crop recommmendation folder and files

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",500))
-    app.run(host="0.0.0.0",port=port)
+    # port = int(os.environ.get("PORT",500))
+    # app.run(host="0.0.0.0",port=)
     # app.run(debug=True)
+    app.run(port=4996)
